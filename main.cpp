f14369ccd50cd3a78f703ab6b44440a1c1e3b327
Yassine <yassineuav@gmail.com>
2022-08-11 21:39:06 -0600

    learn CPP for Unreal Engine

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
-int main() {
+using namespace std;
 
-    std::cout << 2 << std::endl;
+int main() {
+    /*
+     * exercise 1:
+     * you made a sales for 95,000$
+     * state tax = 4%
+     * country tax = 2%
+     * calculate the net income
+     */
+
+    const double state_tax_rate = .04;
+    const double county_tax_rate = .02;
+
+    double sales = 95000;
+
+    double net_income = sales  - (sales * (state_tax_rate + county_tax_rate));
+
+    cout << "Sales: $" << sales << endl;
+
+    cout << net_income << endl;
+
     return 0;
 }
