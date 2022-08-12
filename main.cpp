b72a420c946166d8a2eeb211a5b2b9f537085184
Yassine <yassineuav@gmail.com>
2022-08-11 22:01:49 -0600

    learn CPP for Unreal Engine

--- main.cpp
+++ main.cpp
@@ -3,24 +3,11 @@
 using namespace std;
 
 int main() {
-    /*
-     * exercise 1:
-     * you made a sales for 95,000$
-     * state tax = 4%
-     * country tax = 2%
-     * calculate the net income
-     */
-
-    const double state_tax_rate = .04;
-    const double county_tax_rate = .02;
-
-    double sales = 95000;
-
-    double net_income = sales  - (sales * (state_tax_rate + county_tax_rate));
-
-    cout << "Sales: $" << sales << endl;
-
-    cout << net_income << endl;
+    cout << "Fahrenheit: ";
+    int fahrenheit;
+    cin >> fahrenheit;
+    double celsius = (fahrenheit - 32 )/ 1.8;
+    cout << celsius;
 
     return 0;
 }
